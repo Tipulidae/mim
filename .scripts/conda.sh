46ccf978b8028624d0177b4dcc15bc7686436552
Tipulidae <axel.nystrom@gmail.com>
2020-12-01 15:59:47 +0100

    Reverts back to the old system with two conda envs
    Apparently I was mistaken with not needing two versions of tensorflow
    when using CPU or GPU, so I reverted back to the old "system", except
    now GPU is default and there's a separate mim-cpu environment. Also had
    to move jupyter_contrib_nbextensions to the pip-section of the yml file
    for everything to work. But now it does work, I think, maybe.

--- .scripts/conda.sh
+++ .scripts/conda.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-echo "Updating your conda environment..."
+echo "Creating or updating conda environments. There are separate environments
+depending on if you want to use tensorflow with or without gpu. Select which
+environments to create: (1) CPU only (2) GPU (3) Both CPU and GPU environments"
 
-conda env update --name mim --file environment.yml --prune
+read answer
 
-echo "Done! Activate the environment by typing 'conda activate mim'."
+if [[ $answer = 1 ]]
+then
+  conda env update --name mim-cpu --file environment_cpu.yml --prune
+elif [[ $answer = 2 ]]
+then
+  conda env update --name mim --file environment.yml --prune
+else
+  conda env update --name mim-cpu --file environment_cpu.yml --prune
+  conda env update --name mim --file environment.yml --prune
+fi
