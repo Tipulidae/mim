b156e231ca3eb0c93b294cc346cc9ba57632ba06
Tipulidae <axel.nystrom@gmail.com>
2020-12-01 09:58:14 +0100

    Fixes the conda environments
    Apparently with tensorflow 2.3 you no longer need separate versions of
    tensorflow if you want to run without gpu. So now I only require the one
    conda environment.
    
    I also fixed so that pytest looks only in the tests folder when
    collecting tests, which speeds the whole thing up with 10s (and you
    don't need to write pytest tests, you can write only pytests as usual).

--- .scripts/conda.sh
+++ .scripts/conda.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-echo "Creating or updating conda environments. There are separate environments
-depending on if you want to use tensorflow with or without gpu. Select which
-environments to create: (1) CPU only (2) GPU (3) Both CPU and GPU environments"
+echo "Updating your conda environment..."
 
-read answer
+conda env update --name mim --file environment.yml --prune
 
-if [[ $answer = 1 ]]
-then
-  conda env update --name mim --file environment.yml --prune
-elif [[ $answer = 2 ]]
-then
-  conda env update --name mim-gpu --file environment_gpu.yml --prune
-else
-  conda env update --name mim --file environment.yml --prune
-  conda env update --name mim-gpu --file environment_gpu.yml --prune
-fi
+echo "Done! Activate the environment by typing 'conda activate mim'."
