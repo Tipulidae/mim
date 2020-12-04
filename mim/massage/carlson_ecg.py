4dd953f89d96a87aae357486c48520e3eaa1582f
Tipulidae <axel.nystrom@gmail.com>
2020-12-04 10:34:51 +0100

    Adds WIP function to evaluate status of ECGs
    Can check if certain fields are missing, as well as if the diagnosis
    indicates a bad ECG (like lead reversals etc).

--- mim/massage/carlson_ecg.py
+++ mim/massage/carlson_ecg.py
@@ -2,22 +2,111 @@ from enum import Enum
 
 
 class ECGStatus(Enum):
-    MISSING_FIELD_DATA = 0
-    MISSING_FIELD_LABELS = 1
-    MISSING_FIELD_ECG = 2
-    MISSING_FIELD_BEAT = 3
+    MISSING_DATA = 0
+    MISSING_LABELS = 1
+    MISSING_ECG = 2
+    MISSING_BEAT = 3
+
+    MISSING_RHYTHM = 4
+    MISSING_DIAGNOSES = 5
+    MISSING_SUMMARY = 6
+    MISSING_MEASUREMENTS = 7
+
+    TECHNICAL_ERROR = 8
+    BAD_DIAGNOSIS = 9
+
+    FILE_MISSING = 10
 
 
 def status_of_ecg_dict(ecg):
     status = set()
     if 'Data' not in ecg:
-        status.add(ECGStatus.MISSING_FIELD_DATA)
-    else:
-        if 'Labels' not in ecg['Data']:
-            status.add(ECGStatus.MISSING_FIELD_LABELS)
-        if 'ECG' not in ecg['Data']:
-            status.add(ECGStatus.MISSING_FIELD_ECG)
-        if 'Beat' not in ecg['Data']:
-            status.add(ECGStatus.MISSING_FIELD_BEAT)
+        status.add(ECGStatus.MISSING_DATA)
+    elif dtypes := ecg['Data'].dtype.fields:
+        if 'Labels' not in dtypes:
+            status.add(ECGStatus.MISSING_LABELS)
+        else:
+            # TODO: Check that labels are good
+            pass
+
+        if 'ECG' not in dtypes:
+            status.add(ECGStatus.MISSING_ECG)
+        else:
+            # TODO: Check dimensions of ECG
+            # TODO: Check for empty columns and rows
+            pass
+
+        if 'Beat' not in dtypes:
+            status.add(ECGStatus.MISSING_BEAT)
+        else:
+            # TODO: Check dimensions of Beat
+            # TODO: Check for empty columns and rows
+            pass
+
+    if 'Measurements' not in ecg:
+        status.add(ECGStatus.MISSING_MEASUREMENTS)
+    elif dtypes := ecg['Measurements'].dtype.fields:
+        if 'D' not in dtypes or is_empty_nested(ecg['Measurements']['D']):
+            status.add(ECGStatus.MISSING_DIAGNOSES)
+        else:
+            diagnoses = flatten_nested(ecg['Measurements']['D'])
+            if contains_bad_diagnosis(diagnoses):
+                status.add(ECGStatus.BAD_DIAGNOSIS)
+
+        if 'R' not in dtypes or is_empty_nested(ecg['Measurements']['R']):
+            status.add(ECGStatus.MISSING_RHYTHM)
+        if 'S' not in dtypes or is_empty_nested(ecg['Measurements']['S']):
+            status.add(ECGStatus.MISSING_SUMMARY)
+        else:
+            if 'Technical error' in flatten_nested(ecg['Measurements']['S']):
+                status.add(ECGStatus.TECHNICAL_ERROR)
 
     return status
+
+
+def is_empty_nested(data):
+    return len(data[0][0]) == 0
+
+
+def flatten_nested(data):
+    if is_empty_nested(data):
+        return []
+    else:
+        return [x[0] for x in data[0][0][0]]
+
+
+def contains_bad_diagnosis(diagnoses):
+    for diagnose in diagnoses:
+        if is_bad_diagnose(diagnose):
+            return True
+    return False
+
+
+def is_bad_diagnose(diagnose):
+    return (
+        diagnose.startswith("Lead(s) unsuitable for analysis:") or
+        diagnose in bad_diagnoses
+    )
+
+
+bad_diagnoses = {
+    '--- No further analysis made ---',
+    '--- Possible arm lead reversal - hence only aVF, V1-V6 analyzed ---',
+    '--- Possible arm/leg lead interchange - hence only V1-V6 analyzed ---',
+    '--- Possible limb lead reversal - hence only V1-V6 analyzed ---',
+    '--- Possible measurement error ---',
+    '--- Similar QRS in V leads ---',
+    '--- Suggests dextrocardia ---',
+    '--- Technically unsatisfactory tracing ---'
+    'IV conduction defect',
+    'Possible faulty V2 - omitted from analysis',
+    'Possible faulty V3 - omitted from analysis',
+    'Possible faulty V4 - omitted from analysis',
+    'Possible faulty V5 - omitted from analysis',
+    'Possible sequence error: V1,V2 omitted',
+    'Possible sequence error: V2,V3 omitted',
+    'Possible sequence error: V3,V4 omitted',
+    'Possible sequence error: V4,V5 omitted',
+    'V1/V2 are at least one interspace too high and have been omitted from '
+    'the analysis'
+}
--- tests/massage/test_carlson_ecg.py
+++ tests/massage/test_carlson_ecg.py
@@ -1,39 +1,108 @@
-from mim.massage.carlson_ecg import ECGStatus, status_of_ecg_dict
+import numpy as np
+
+from mim.massage.carlson_ecg import (
+    ECGStatus,
+    status_of_ecg_dict,
+    flatten_nested
+)
 
 
 def test_can_detect_missing_data_field():
-    ecg = {'Patient': [], 'Recording': [], 'Measurements': []}
-    assert ECGStatus.MISSING_FIELD_DATA in status_of_ecg_dict(ecg)
+    ecg = {
+        'Patient': np.array([]),
+        'Recording': np.array([]),
+        'Measurements': np.array([])
+    }
+    assert ECGStatus.MISSING_DATA in status_of_ecg_dict(ecg)
 
-    ecg['Data'] = []
-    assert ECGStatus.MISSING_FIELD_DATA not in status_of_ecg_dict(ecg)
+    ecg['Data'] = np.array([])
+    assert ECGStatus.MISSING_DATA not in status_of_ecg_dict(ecg)
 
 
 def test_can_detect_missing_data_field_details():
     ecg = {
-        'Patient': [],
-        'Recording': [],
-        'Measurements': [],
-        'Data': {
-            'Foo': [],
-            'Bar': []
-        }
+        'Patient': np.array([]),
+        'Recording': np.array([]),
+        'Measurements': np.array([]),
+        'Data':
+            np.array(
+                ([], [], []),
+                dtype=[('Foo', 'O'), ('Bar', 'O'), ('Baz', 'O')]
+            )
     }
-    assert ECGStatus.MISSING_FIELD_LABELS in status_of_ecg_dict(ecg)
-    assert ECGStatus.MISSING_FIELD_ECG in status_of_ecg_dict(ecg)
-    assert ECGStatus.MISSING_FIELD_BEAT in status_of_ecg_dict(ecg)
 
-    ecg['Data']['Labels'] = []
-    assert ECGStatus.MISSING_FIELD_LABELS not in status_of_ecg_dict(ecg)
-    assert ECGStatus.MISSING_FIELD_ECG in status_of_ecg_dict(ecg)
-    assert ECGStatus.MISSING_FIELD_BEAT in status_of_ecg_dict(ecg)
+    assert ECGStatus.MISSING_LABELS in status_of_ecg_dict(ecg)
+    assert ECGStatus.MISSING_ECG in status_of_ecg_dict(ecg)
+    assert ECGStatus.MISSING_BEAT in status_of_ecg_dict(ecg)
 
-    ecg['Data']['ECG'] = []
-    assert ECGStatus.MISSING_FIELD_LABELS not in status_of_ecg_dict(ecg)
-    assert ECGStatus.MISSING_FIELD_ECG not in status_of_ecg_dict(ecg)
-    assert ECGStatus.MISSING_FIELD_BEAT in status_of_ecg_dict(ecg)
+    ecg['Data'] = np.array(
+        ([], [], []),
+        dtype=[('Labels', 'O'), ('Bar', 'O'), ('Baz', 'O')]
+    )
+    assert ECGStatus.MISSING_LABELS not in status_of_ecg_dict(ecg)
+    assert ECGStatus.MISSING_ECG in status_of_ecg_dict(ecg)
+    assert ECGStatus.MISSING_BEAT in status_of_ecg_dict(ecg)
 
-    ecg['Data']['Beat'] = []
-    assert ECGStatus.MISSING_FIELD_LABELS not in status_of_ecg_dict(ecg)
-    assert ECGStatus.MISSING_FIELD_ECG not in status_of_ecg_dict(ecg)
-    assert ECGStatus.MISSING_FIELD_BEAT not in status_of_ecg_dict(ecg)
+    ecg['Data'] = np.array(
+        ([], [], []),
+        dtype=[('Labels', 'O'), ('ECG', 'O'), ('Baz', 'O')]
+    )
+    assert ECGStatus.MISSING_LABELS not in status_of_ecg_dict(ecg)
+    assert ECGStatus.MISSING_ECG not in status_of_ecg_dict(ecg)
+    assert ECGStatus.MISSING_BEAT in status_of_ecg_dict(ecg)
+
+    ecg['Data'] = np.array(
+        ([], [], []),
+        dtype=[('Labels', 'O'), ('ECG', 'O'), ('Beat', 'O')]
+    )
+    assert ECGStatus.MISSING_LABELS not in status_of_ecg_dict(ecg)
+    assert ECGStatus.MISSING_ECG not in status_of_ecg_dict(ecg)
+    assert ECGStatus.MISSING_BEAT not in status_of_ecg_dict(ecg)
+
+
+def test_can_detect_missing_measurements():
+    ecg = {'Patient': [], 'Recording': []}
+    assert ECGStatus.MISSING_MEASUREMENTS in status_of_ecg_dict(ecg)
+
+    ecg['Measurements'] = np.array([])
+    assert ECGStatus.MISSING_MEASUREMENTS not in status_of_ecg_dict(ecg)
+
+
+def test_can_detect_missing_diagnoses():
+    ecg = {
+        'Patient': np.array([]),
+        'Recording': np.array([]),
+        'Measurements':
+            np.array(
+                [([[[]]], [[[]]], [[[]]])],
+                dtype=[('Foo', 'O'), ('S', 'O'), ('R', 'O')]
+            )
+    }
+
+    assert ECGStatus.MISSING_DIAGNOSES in status_of_ecg_dict(ecg)
+
+    ecg['Measurements'] = np.array(
+        [(np.array([[[['Hi'], ['There']]]]), [[[]]], [[[]]])],
+        dtype=[('D', 'O'), ('S', 'O'), ('R', 'O')]
+    )
+    assert ECGStatus.MISSING_DIAGNOSES not in status_of_ecg_dict(ecg)
+
+
+def test_flatten_ridiculous_non_empty_nesting():
+    data = [[[[
+        ["A fly can't bird"],
+        ["But a bird can fly"]
+    ]]]]
+
+    expected = ["A fly can't bird", "But a bird can fly"]
+    actual = flatten_nested(data)
+    assert actual == expected
+
+
+def test_flatten_ridiculous_empty_nesting():
+    data = [[[
+    ]]]
+
+    expected = []
+    actual = flatten_nested(data)
+    assert actual == expected
