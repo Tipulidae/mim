d20eca2f5c5991250ab1d345bc046915dfd8383f
Tipulidae <axel.nystrom@gmail.com>
2021-01-14 15:02:55 +0100

    Adds EscTrop extractor
    Very simple structure thus far, more to come.

---
+++ mim/extractors/esc_trop.py
@@ -0,0 +1,28 @@
+from mim.massage.esc_trop import make_ed_table, make_troponin_table
+from mim.extractors.extractor import Data, Container, ECGData
+
+
+class EscTrop:
+    def __init__(self, specification):
+        self.specification = specification
+
+    def get_data(self):
+        ed = make_ed_table()
+        tnt = make_troponin_table()
+        ed = ed.join(tnt).reset_index()
+
+        ecg_path = '/mnt/air-crypt/air-crypt-esc-trop/axel/ecg.hdf5'
+
+        data = Container(
+            {
+                'x': ECGData(
+                    ecg_path,
+                    mode='beat',
+                    index=ed.ecg_id.astype(int).values
+                ),
+                'y': Data(ed.mace_30_days.astype(int).values)
+            },
+            index=ed.index
+        )
+
+        return data
